98c2facc153b36f2d996dd5a45b01202da7bd331
Windows10 <developermuhammadrizo@gmail.com>
2022-01-21 18:09:29 +0500

    recycler view with nested scroll view in java

---
+++ app/src/main/java/com/example/project6/activities/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.project6.activities;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project6.R;
+import com.example.project6.adapters.CustomAdapter;
+import com.example.project6.models.Member;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        refreshAdapter(prepareMemberList());
+
+    }
+
+    void initViews() {
+        context = this;
+        recyclerView = findViewById(R.id.recycler_view);
+    }
+
+    void refreshAdapter(ArrayList<Member> members) {
+        CustomAdapter customAdapter = new CustomAdapter(context, members);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setNestedScrollingEnabled(true);
+    }
+
+    ArrayList<Member> prepareMemberList() {
+        ArrayList<Member> members = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++) {
+            members.add(new Member("Muhammadrizo" + i, "Nurullaxo'jayev" + i));
+        }
+        return members;
+    }
+
+}
+
